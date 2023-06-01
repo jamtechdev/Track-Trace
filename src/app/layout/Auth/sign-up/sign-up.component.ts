7c07549562a7df192b0a63d7d3902709d3933a6a
Divyanshu Mishra <jamtechba@gmail.com>
2023-06-01 14:00:22 +0530

    add controls sign up form

--- src/app/layout/Auth/sign-up/sign-up.component.ts
+++ src/app/layout/Auth/sign-up/sign-up.component.ts
@@ -3,16 +3,22 @@ import { Router } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { UserServicesService } from 'src/app/service/user-services.service';
 // import { UserServicesService } from '../service/user-services.service';
+import { OnChanges , OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
-export class SignUpComponent {
-
+export class SignUpComponent implements OnInit {
   constructor(private router: Router, public service: UserServicesService) {}
+   
   databsdismiss = '';
+
+  ngOnInit(): void {
+    console.log(this.f , 'jsdjd');
+    
+  }
 
   signUpForm = new FormGroup({
     email: new FormControl('', [
@@ -29,10 +35,19 @@ export class SignUpComponent {
     password_confirmation:new FormControl('', [
       Validators.required,
     ]),
-  });
+     
+        
+
+  } , );
+
+  
+  
+ get f() {
+    return this.signUpForm.controls;
+  }
 
   onSubmit() {
-    console.log(this.signUpForm.value);
+    console.log(this.signUpForm);
     // this.service.setUser(this.signUpForm.value).subscribe(res=>console.log(res)
     // );
     this.router.navigate(['/home']);
