6557cce8880507318b8ec0f237a37b7be6459116
Divyanshu Mishra <jamtechba@gmail.com>
2023-06-02 15:44:19 +0530

    route guard added for auth & home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
 import { LoginComponent } from './layout/Auth/login/login.component';
+import { authGuard } from './common-resources/servieces/gaurd/auth.guard';
+import { logInGaurd } from './common-resources/servieces/gaurd/login.guard';
 
 const routes: Routes = [
   {
     path: '',
+     canActivate:[logInGaurd],
     loadChildren: () =>
       import('./layout/Auth/auth.module').then((m) => m.AuthModule),
   }
   ,  {
     path: 'home',
+    canActivate:[authGuard],
+
     loadChildren: () =>
       import('./layout/layout.module').then((m) => m.LayoutModule),
   },
