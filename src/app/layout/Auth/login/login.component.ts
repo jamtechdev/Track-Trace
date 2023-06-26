4189bb52f776b090cb24edcdafa0d4f9f4e54d3a
Divyanshu Mishra <jamtechba@gmail.com>
2023-06-26 17:25:30 +0530

    login password format validation

--- src/app/layout/Auth/login/login.component.ts
+++ src/app/layout/Auth/login/login.component.ts
@@ -52,9 +52,9 @@ export class LoginComponent implements OnInit {
         '',
         [
           Validators.required,
-          // Validators.pattern(
-          //   '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-zd$@$!%*?&].{8,}$'
-          // ),
+          Validators.pattern(
+            '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-zd$@$!%*?&].{8,}$'
+          ),
         ],
       ],
     });
